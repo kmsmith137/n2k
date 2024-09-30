203aebbdfe6f057a6116f3ef3db75ffde33f4a0a
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2024-09-30 02:46:16 +0000

    test-sk-bias cleanups

--- src_bin/test-sk-bias.cu
+++ src_bin/test-sk-bias.cu
@@ -1,5 +1,6 @@
 #include "../include/n2k.hpp"
 #include "../include/n2k/interpolation.hpp"
+#include "../include/n2k/SkKernel.hpp"
 
 #include <gputils/string_utils.hpp>
 #include <iostream>
@@ -8,6 +9,7 @@ using namespace std;
 using namespace n2k;
 
 
+// FIXME move somewhere more general?
 static bool is_perfect_square(int n)
 {
     if (n < 1)
@@ -16,112 +18,7 @@ static bool is_perfect_square(int n)
     return n == m*m;
 }
 
-
-// FIXME move to libn2k.so (needs GPU-friendly interface)
-template<typename T>
-struct Bvec
-{
-    int n = 0;
-    const double *bias_coeffs = nullptr;
-    
-    int min_s1 = 0;
-    int max_s1 = 0;
-
-    // Length 98*n+1
-    vector<T> bvec;
-
-    // Helper function for constructor.
-    // No range check on y!
-    inline double _eval_y(int ix, double y)
-    {
-	constexpr int nx = sk_globals::bias_nx;
-	constexpr int ny = sk_globals::bias_ny;
-
-	assert((ix >= 0) && (ix < nx));
-	
-	double ret = 0.0;
-	double ypow = 1.0;
-	
-	for (int j = 0; j < ny; j++) {
-	    ret += bias_coeffs[ix*ny+j] * ypow;
-	    ypow *= y;
-	}
-
-	return ret;
-    }
-	
-    
-    // Helper function for constructor.
-    inline double _interpolate(double x, double y)
-    {
-	constexpr float xmin = sk_globals::xmin;
-	constexpr float xmax = sk_globals::xmax;
-	constexpr int nx = sk_globals::bias_nx;
-	
-	// Normalize to [0,nx-1]
-	double t = (nx-1) * (x-xmin) / (xmax-xmin);
-	assert(t >= 0.9);
-	assert(t <= nx-1.9);
-	
-	int it = int(t);
-	it = std::max(it,1);
-	it = std::min(it,nx-3);
-
-	double b0 = _eval_y(it-1, y);
-	double b1 = _eval_y(it, y);
-	double b2 = _eval_y(it+1, y);
-	double b3 = _eval_y(it+2, y);
-
-	return cubic_interpolate(t-it, b0, b1, b2, b3);
-    }
-    
-    Bvec(int n_, bool verbose=false)
-    {
-	constexpr float mu_min = sk_globals::mu_min;
-	constexpr float mu_max = sk_globals::mu_max;
-	constexpr int n_min = sk_globals::bias_nmin;
-	
-	this->n = n_;
-	this->bias_coeffs = sk_globals::get_bsigma_coeffs();
-	assert(n >= n_min);
-
-	double eps = 5.0e-11;
-	this->min_s1 = n * mu_min;
-        this->min_s1 = int((1-eps)*min_s1 + 1);  // round up
-        this->max_s1 = n * mu_max;
-        this->max_s1 = int((1+eps)*max_s1);      // round down
-
-	if (verbose) {
-	    cout << "min_s1 = " << min_s1 << endl;
-	    cout << "max_s1 = " << max_s1 << endl;
-	}
-	
-	assert(min_s1 > 0);
-	assert(min_s1 <= max_s1);
-	assert(max_s1 <= 98*n);
-	
-	this->bvec = vector<T> (98*n+1, 0);
-
-	// FIXME in hindsight, interpolation could be done more efficiently in two
-	// steps. First, take the 2-d shape-(128,4) coefficient array, and "evaluate y"
-	// to get a 1-d length-128 array. Then, "resample x" by interpolation, to get
-	// a 1-d length-(98*n+1) array.
-
-	for (int s1 = min_s1; s1 <= max_s1; s1++) {
-	    double mu = s1/double(n);
-	    double x = log(mu);
-	    double y = 1.0/n;
-	    bvec[s1] = _interpolate(x, y);
-
-	    if (verbose)
-		cout << "   bvec[" << s1 << "] = " << bvec[s1] << endl;
-	}
-    }
-};
-
-
-
-// FIXME move to gputils!
+// FIXME move somewhere more general?
 inline int quantize(double x)
 {
     x = std::abs(x);
@@ -133,7 +30,6 @@ inline int quantize(double x)
 int main(int argc, char **argv)
 {
     std::mt19937 &rng = gputils::default_rng;
-    bool verbose = false;
     
     if (argc != 3) {
 	cerr << "Usage: test-sk-bias <rms> <n>" << endl;
@@ -141,36 +37,51 @@ int main(int argc, char **argv)
     }
 
     double rms = gputils::from_str<double> (argv[1]);
-    int n = gputils::from_str<int> (argv[2]);
+    long n = gputils::from_str<long> (argv[2]);
     cout << "Running Monte Carlos with rms=" << rms << ", n=" << n << endl;
+    
+    long min_s1 = n * sk_globals::mu_min;
+    long max_s1 = n * sk_globals::mu_max;
+    
+    double eps = 5.0e-11;
+    min_s1 = long((1-eps)*min_s1 + 1);  // round up
+    max_s1 = long((1+eps)*max_s1);      // round down
 
-    std::normal_distribution<double> dist(0, rms);
-    Bvec<double> bvec(n, verbose);
+    // Precompute biases for all possible values of S1.
+    // (This is faster than doing interpolation in the inner loop.)
+
+    vector<double> bvec(98*n+1, 0);
+    for (long s1 = min_s1; s1 <= max_s1; s1++) {
+	double x = log(double(s1) / double(n));
+	double y = 1.0 / double(n);
+	bvec[s1] = interpolate_sk_bias(x,y);
+    }
 
     long nmc = 0;
     double sum_sk = 0.0;
     double sum_sk2 = 0.0;
+    std::normal_distribution<double> dist(0, rms);
 
     // Run forever!
     for (long nouter = 1; 1; nouter++) {
-	int s1 = 0;
-	int s2 = 0;
+	long s1 = 0;
+	long s2 = 0;
 
-	for (int i = 0; i < n; i++) {
-	    int ex = quantize(dist(rng));
-	    int ey = quantize(dist(rng));
-	    int e2 = ex*ex + ey*ey;
+	for (long i = 0; i < n; i++) {
+	    long ex = quantize(dist(rng));
+	    long ey = quantize(dist(rng));
+	    long e2 = ex*ex + ey*ey;
 	    s1 += e2;
 	    s2 += e2*e2;
 	}
 	    
-	if ((s1 < bvec.min_s1) || (s1 > bvec.max_s1))
+	if ((s1 < min_s1) || (s1 > max_s1))
 	    continue;
 
 	double ds1 = double(s1);
 	double sk = n*s2/(ds1*ds1) - 1;
 	sk *= (n+1) / double(n-1);
-	sk -= bvec.bvec[s1];
+	sk -= bvec[s1];
 	
 	nmc++;
 	sum_sk += sk;
