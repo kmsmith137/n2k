6c820725ae6e41873d2714b23947b051f53a7e2f
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2024-10-06 18:41:23 +0000

    time-pl-kernels

---
+++ src_bin/time-pl-kernels.cu
@@ -0,0 +1,38 @@
+#include "../include/n2k/pl_kernels.hpp"
+#include <gputils/CudaStreamPool.hpp>
+
+using namespace std;
+using namespace gputils;
+using namespace n2k;
+
+
+static void time_correlate_pl(const string &name, long T, long F, long S, long Nds, long ninner)
+{
+    const long nouter = 10;
+    
+    long Tout = T / Nds;
+    long ntiles = ((S/16) * ((S/16)+1)) / 2;
+
+    Array<ulong> pl({T/64, F, S}, af_gpu | af_zero);
+    Array<int> v({Tout,F,ntiles,16,16}, af_gpu | af_zero);
+    double gb = 1.0e-9 * ninner * (8*pl.size + 4*v.size);
+
+    auto callback = [&](const CudaStreamPool &pool, cudaStream_t stream, int istream)
+    {
+	for (long i = 0; i < ninner; i++)
+	    launch_correlate_pl_kernel(v, pl, Nds, stream);  // calls CUDA_PEEK()
+    };
+
+    CudaStreamPool sp(callback, nouter, 1, name);
+    sp.monitor_throughput("Global memory BW (GB/s)", gb);
+    sp.run();
+}
+
+
+int main(int argc, char **argv)
+{
+    // (T,F) artificially increased.
+    time_correlate_pl("CHORD pathfinder", 192*1024, 64*1024, 16, 128*48, 5);
+    
+    return 0;
+}
